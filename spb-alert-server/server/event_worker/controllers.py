058ecec1b1b56a86da2a030c5d241f278f86ab4f
Ilia <i.ilmenskiy@corp.vk.com>
2020-11-14 14:04:08 +0300

    added pushing to event service

--- spb-alert-server/server/event_worker/controllers.py
+++ spb-alert-server/server/event_worker/controllers.py
@@ -1,15 +1,26 @@
-import logging
+import json
 from tornado.web import RequestHandler
+from server.event_worker.worker import Worker
 
 
 class TestHandler(RequestHandler):
 
     @staticmethod
     def get_mapping():
-        return "/test"
+        return "/internal.appendEvent"
 
     def data_received(self, chunk):
         pass
 
-    def get(self, *args, **kwargs):
-        logging.warning("accepted get request to Test Handler")
+    def post(self, *args, **kwargs):
+        event = json.loads(self.request.body)
+        event = ApiEvent(event)
+        Worker.push(event)
+
+
+class ApiEvent:
+    def __init__(self, json_obj):
+        self.event_type = json_obj["event_type"]
+        self.timestamp = json_obj["timestamp"]
+        self.lat = json_obj["lat"]
+        self.lon = json_obj["lon"]
